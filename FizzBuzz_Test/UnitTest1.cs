355fddc239944c814b10d54e4f3c60a53c668b1a
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-19 17:33:30 +0200

    Added failing fizz test

--- FizzBuzz_Test/UnitTest1.cs
+++ FizzBuzz_Test/UnitTest1.cs
@@ -6,13 +6,24 @@ namespace FizzBuzz_Test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [DataTestMethod]
         [DataRow(2)]
         [DataRow(5)]
+        [DataRow(19)]
+        [DataRow(12)]
         [DataRow(87)]
         public void Converting_A_Number_Not_Divisible_By_3_or_5_Returns_That_Number(int number)
         {
             Assert.AreEqual(Program.Convert(number), number.ToString()) ;
+        }
+
+        [DataTestMethod]
+        [DataRow(3)]
+        public void Converting_A_Number_Only_Divisible_By_3_But_Not_By_5_Returns_Fizz(int number)
+        {
+            Assert.AreEqual(
+                Program.Convert(number), "Fizz"
+            );
         }
     }
 }
