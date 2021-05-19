019ec703592ae01b2338ec0be1172848620f16b9
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-19 17:45:31 +0200

    add experiment for isDivisibleBy

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,13 +8,19 @@ namespace FizzBuzz
         {
             Console.WriteLine("Hello World!");
         }
+
+        private static bool isDivisibleBy(int nom, int denom)
+        {
+            return nom % denom == 0;
+        }
+
         public static string Convert(int number)
         {
-            if (number % 3 == 0)
+            if (isDivisibleBy(number, 3))
             {
                 return "Fizz";
             }
-            else if (number % 5 == 0)
+            else if (isDivisibleBy(number, 5))
             {
                 return "Buzz";
             }
